f1f007e936b9bd7c6704ca2b1c9ff1a3adaeb841
oluwadarasimi <Adewoledarasimi25@gmail.com>
2023-09-10 08:35:43 -0400

    laravel-API-commit-1

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Product::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+
+    {
+        $request->validate([
+            'name' => 'required',
+            'slug' => 'required',
+            'description' => 'required',
+            'price' => 'required'
+        ]);
+
+        return Product::create([
+            'name' => $request->name,
+            'slug' => $request->slug,
+            'description' => $request->description,
+            'price' => $request->price
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return Product::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
